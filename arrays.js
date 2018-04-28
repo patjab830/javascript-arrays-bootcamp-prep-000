27bf071d75f6064a5a26f3559aeb7727488f2b4e
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 04:47:07 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+function addElementToBeginningOfArray(arr, element) {
+  var result = [element, ...arr];
+  return result;
+}
+
+function destructivelyAddElementToBeginningofArray(arr, element) {
+  
+}
