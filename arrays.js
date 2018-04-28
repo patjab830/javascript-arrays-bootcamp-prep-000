e301343f0c6c92b975cb1881ccd1a539ab3556ca
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 05:10:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
 function addElementToBeginningOfArray(arr, element) {
   var result = [element, ...arr];
   return result;
